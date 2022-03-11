ef4c035e7a0ae328da490e419ad0db3a00d56284
Milad Nekofar <milad@nekofar.com>
2022-03-11 23:00:34 +0330

    build: add `qodana` plugin for code inspection

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ plugins {
     id("org.jetbrains.intellij") version "1.4.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
+    // Gradle Qodana Plugin
+    id("org.jetbrains.qodana") version "0.1.13"
 }
 
 group = properties("pluginGroup")
