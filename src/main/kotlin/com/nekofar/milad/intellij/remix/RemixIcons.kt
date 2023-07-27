94287095dc17e863851dcb87b76a356e91ec1dbd
Milad Nekofar <milad@nekofar.com>
2023-07-27 15:25:42 +0330

    refactor: update icons class for improved readability
    Removed the redundant and unused "FileType" enumeration as well as an unnecessary type declaration in the RemixIcons.kt file. These changes were made to streamline the code, efficiently utilize imports, and to enhance the readability and maintainability of the codebase.

--- src/main/kotlin/com/nekofar/milad/intellij/remix/RemixIcons.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/remix/RemixIcons.kt
@@ -1,13 +1,8 @@
 package com.nekofar.milad.intellij.remix
 
 import com.intellij.openapi.util.IconLoader
-import javax.swing.Icon
 
-@Suppress("unused")
 object RemixIcons {
     @JvmField
-    val FileType = IconLoader.getIcon("/icons/type.svg", javaClass)
-
-    @JvmField
-    val ProjectGenerator: Icon = IconLoader.getIcon("/icons/remix.svg", javaClass)
+    val ProjectGenerator = IconLoader.getIcon("/icons/remix.svg", javaClass)
 }
