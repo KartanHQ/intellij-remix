d303385243e5c40c02fc785d1e317ca1dd2adf37
Milad Nekofar <milad@nekofar.com>
2023-07-28 10:28:57 +0330

    refactor: add back file type icon to icons
    Added a new FileType icon in RemixIcons.kt for better file type representation in the UI. The `@Suppress("unused")` annotation was added to prevent possible warning messages regarding unused fields.

--- src/main/kotlin/com/nekofar/milad/intellij/remix/RemixIcons.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/remix/RemixIcons.kt
@@ -2,7 +2,11 @@ package com.nekofar.milad.intellij.remix
 
 import com.intellij.openapi.util.IconLoader
 
+@Suppress("unused")
 object RemixIcons {
+    @JvmField
+    val FileType = IconLoader.getIcon("/icons/type.svg", javaClass)
+
     @JvmField
     val ProjectGenerator = IconLoader.getIcon("/icons/remix.svg", javaClass)
 }
